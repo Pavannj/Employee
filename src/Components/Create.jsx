f2ed96932f0dd44fa05bbabc4e389385c5f0f7a0
Pavan Jamagoudar <143714763+Pavannj@users.noreply.github.com>
2024-07-29 09:08:48 +0530

    Add files via upload

---
+++ src/Components/Create.jsx
@@ -0,0 +1,74 @@
+import React, { Children , useState} from "react";
+import Navbar from "./Navbar";
+import "./Create.css";
+import {useNavigate} from 'react-router-dom'
+
+function Create() {
+let navigate =useNavigate()
+const [name,setName]=useState("")
+const [email,setEmail]=useState("")
+const [number,setNumber]=useState("")
+const [designation,setDesignation]=useState("")
+const [gender,setGender]=useState("")
+const [course,setCourse]=useState("")
+const [image,setImage]=useState("")
+
+
+let handleSubmit=(e)=>{
+  e.preventDefault()
+  let newEmployee={name,email,number,designation,gender,course,image}
+  fetch("http://localhost:3000/Data.json",
+    {
+      method:"POST",
+      headers:{"content-type":"application/json"},
+      body:JSON.stringify(newEmployee)
+    }
+  ).then(()=>{
+    alert('Added');
+    navigate("/dashboard")
+  })
+}
+
+  return (
+    <>
+      <Navbar />
+      <h2>Create New Employee</h2>
+      <form action="" id="cform" onSubmit={handleSubmit}>
+
+        <label htmlFor="" id="clabel">Name</label>
+        <input type="text"  id="cinput" value={name} onChange={(e)=>{
+          setName(e.target.value)
+        }}/>
+        <label htmlFor="" id="clabel">Email</label>
+        <input type="email"  id="cinput" value={email} onChange={(e)=>{
+          setEmail(e.target.value)
+        }}/>
+        <label htmlFor="" id="clabel">Mobile No</label>
+        <input type="tel"  id="cinput" value={number} onChange={(e)=>{
+          setNumber(e.target.value)
+        }}/>
+        <label htmlFor="" id="clabel">Designation</label>
+        <input type="text"  id="cinput" value={designation} onChange={(e)=>{
+          setDesignation(e.target.value)
+        }}/>
+        <label htmlFor="" id="clabel">Gender</label>
+        <input type="text"  id="cinput" value={gender} onChange={(e)=>{
+          setGender(e.target.value)
+        }}/>
+        <label htmlFor="" id="clabel">Course</label>
+        <input type="text"  id="cinput" value={course} onChange={(e)=>{
+          setCourse(e.target.value)
+        }}/>
+        <label htmlFor="" id="clabel">Image</label>
+        <input type="url"  id="cinput" value={image} onChange={(e)=>{
+          setImage(e.target.value)
+        }}/>
+      </form>
+      <button  onClick={handleSubmit} id="cbutton">Submit</button>
+   
+
+    </>
+  );
+}
+
+export default Create;
---
+++ src/Components/Details.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import Navbar from "./Navbar";
+import { useEffect } from "react";
+import "./Details.css";
+import Create from "./Create.jsx";
+import { Link, useNavigate  } from "react-router-dom";
+import { BrowserRouter as Router, Routes,Route } from "react-router-dom";
+
+function Details() {
+  
+  const [column, setColumn] = useState([]);
+  const [records, setRecords] = useState([]);
+
+  const navigate =useNavigate()
+
+  useEffect(() => {
+    fetch("http://localhost:3000/Data.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setColumn(Object.keys(data.users[0]));
+        setRecords(data.users);
+      });
+  }, []);
+
+
+  
+  return (
+    
+    <div>
+      <Navbar />
+      <Link to='/create'>
+        <button id="create">Create Employee</button>
+      </Link>
+      <table>
+        <thead>
+          <tr>
+            <td>Unique id</td>
+            <td>Image</td>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Mobile No</td>
+            <td>Role</td>
+            <td>Gender</td>
+            <td>Course</td>
+            <td>Date</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          {records.map((records, i) => (
+            <tr key={i}>
+              <td>{records.id}</td>
+              <td>{records.img}</td>
+              <td>{records.name}</td>
+              <td>{records.email}</td>
+              <td>{records.mobile}</td>
+              <td>{records.designation}</td>
+              <td>{records.gender}</td>
+              <td>{records.course}</td>
+              <td>{records.date}</td>
+              <td><button><Link to='/edit' id="btn">Edit</Link></button><button>Delete</button></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      
+    </div>
+  );
+}
+
+export default Details;
